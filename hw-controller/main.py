e210dba18ab8b9ecafc24ddfd561f728510c9c2d
Catrick-cpu <patrick.streng@web.de>
2026-02-14 12:54:32 +0100

    Adding support for more motors

--- hw-controller/main.py
+++ hw-controller/main.py
@@ -1,13 +1,21 @@
 import argparse
 import time
 import RPi.GPIO as GPIO
+import math as m
 
-STEP_PIN = 17
-DIR_PIN = 27
+
+
+MOTOR_PINS = {
+    1: {"STEP": 17, "DIR": 27},
+    2: {"STEP": 22, "DIR": 23},
+    3: {"STEP": 24,  "DIR": 25},
+    4: {"STEP": 5, "DIR": 6},
+}
+
 
 
 def parse_args():
-    p = argparse.ArgumentParser(description="Einfaches Stepper-Testskript (DE)")
+    p = argparse.ArgumentParser(description="Stepper-Testskript (4 Motoren)")
     p.add_argument(
         "-m",
         "--motorid",
@@ -21,9 +29,9 @@ def parse_args():
         "-sc",
         "--stepcount",
         type=int,
-        default=2000,
+        default=200,
         dest="steps",
-        help="Anzahl Schritte (Standard: 100)",
+        help="Anzahl Schritte (Standard: 200)",
     )
     p.add_argument(
         "-d",
@@ -31,7 +39,7 @@ def parse_args():
         type=float,
         default=0.001,
         dest="delay",
-        help="Pause zwischen Schritten in Sekunden (Standard: 0.01)",
+        help="Pause zwischen Schritten in Sekunden",
     )
     p.add_argument(
         "-dir",
@@ -39,30 +47,38 @@ def parse_args():
         choices=["forward", "backward"],
         default="forward",
         dest="direction",
-        help="Richtung: forward|backward (Standard: forward)",
+        help="Richtung: forward|backward",
     )
     return p.parse_args()
 
 
+
 def setup_gpio():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(STEP_PIN, GPIO.OUT)
-    GPIO.setup(DIR_PIN, GPIO.OUT)
-    GPIO.output(STEP_PIN, GPIO.LOW)
+
+    for motor in MOTOR_PINS.values():
+        GPIO.setup(motor["STEP"], GPIO.OUT)
+        GPIO.setup(motor["DIR"], GPIO.OUT)
+        GPIO.output(motor["STEP"], GPIO.LOW)
 
 
-def move_motor(steps, delay, direction):
-    # Richtung setzen
+
+def move_motor(motor_id, steps, delay, direction):
+
+    step_pin = MOTOR_PINS[motor_id]["STEP"]
+    dir_pin = MOTOR_PINS[motor_id]["DIR"]
+
+    
     if direction == "forward":
-        GPIO.output(DIR_PIN, GPIO.LOW)
+        GPIO.output(dir_pin, GPIO.LOW)
     else:
-        GPIO.output(DIR_PIN, GPIO.HIGH)
+        GPIO.output(dir_pin, GPIO.HIGH)
 
-    # Schritte erzeugen
+    
     for _ in range(steps):
-        GPIO.output(STEP_PIN, GPIO.HIGH)
+        GPIO.output(step_pin, GPIO.HIGH)
         time.sleep(delay)
-        GPIO.output(STEP_PIN, GPIO.LOW)
+        GPIO.output(step_pin, GPIO.LOW)
         time.sleep(delay)
 
 
@@ -72,14 +88,14 @@ def main():
     print(
         f"Motor-ID: {args.motorid}, "
         f"Schritte: {args.steps}, "
-        f"Verzögerung: {args.delay}, "
+        f"Delay: {args.delay}, "
         f"Richtung: {args.direction}"
     )
 
     setup_gpio()
 
     try:
-        move_motor(args.steps, args.delay, args.direction)
+        move_motor(args.motorid, args.steps, args.delay, args.direction)
     finally:
         GPIO.cleanup()
 
